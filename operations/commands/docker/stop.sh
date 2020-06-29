e16dcf9e4decbdb2628b1f3cd003d8a70c561949
Bricx Carasco <bricxraincarasco21@gmail.com>
2020-06-29 16:13:16 +0800

    Create run/commands for console ui and change devops structure

---
+++ operations/commands/docker/stop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Maintainer: Bricx Carasco bricxcarasco.gss@gmail.com
+
+RED='\033[0;31m'
+LIGHTCYAN='\033[1;36m'
+
+echo "\n${RED}Stopping all containers ...${NOCOLOR}\n"
+docker stop laravel-php laravel-nginx-server laravel-phpmyadmin laravel-mysql-server
+
+echo "\n${LIGHTCYAN}Process done ...${NOCOLOR}\n"
