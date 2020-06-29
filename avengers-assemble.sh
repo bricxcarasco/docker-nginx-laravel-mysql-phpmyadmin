7fe2ce7489200ca173dd43ed94358e3ac26d2fdd
Bricx Carasco <bricxraincarasco21@gmail.com>
2020-06-29 10:33:48 +0800

    Add containers npm, composer and artisan

--- avengers-assemble.sh
+++ avengers-assemble.sh
@@ -4,19 +4,24 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NOCOLOR='\033[0m'
 
-echo "${RED}Stopping containers ...${NOCOLOR}"
-
+echo "\n${RED}Stopping containers ...${NOCOLOR}\n"
 docker stop laravel-php laravel-nginx-server laravel-mysql-server laravel-phpmyadmin
 
-echo "${GREEN}Removing containers ...${NOCOLOR}"
-
+echo "\n${GREEN}Removing containers ...${NOCOLOR}\n"
 docker rm laravel-php laravel-nginx-server laravel-mysql-server laravel-phpmyadmin
 
-echo "${YELLOW}Build and up all containers ...${NOCOLOR}"
+echo "\n${YELLOW}Build and up all containers ...${NOCOLOR}\n"
+docker-compose up -d --build
 
-docker-compose build && docker-compose up -d
+if [ -d "./src/vendor" ] 
+then
+    echo "\n${RED}Composer install running..${NOCOLOR}\n"
+    docker-compose run --rm composer install
+fi
 
+echo "\n${YELLOW}Clear views, routes, configurations and caches ...${NOCOLOR}\n"
+docker-compose run --rm artisan optimize
 sudo chmod 777 -R src/storage/ src/bootstrap/cache/
 
-echo "${GREEN}Process done ...${NOCOLOR}"
-echo "${YELLOW}Happy coding !! :-)${NOCOLOR}"
+echo "\n${GREEN}Process done ...${NOCOLOR}\n"
+echo "${YELLOW}Happy coding !! :-)${NOCOLOR}\n"
